81ed277b544b34de8b9a05a3734eb4ca973f7858
andev03 <nguyenhoangan03.work@gmail.com>
2025-11-03 13:02:46 +0700

    feat: setup basic api gateway

---
+++ Routing/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
